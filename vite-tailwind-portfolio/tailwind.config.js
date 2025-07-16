f8b3a1511fd5af693be769cf61ec09b143d71817
louis-ys <lye98055@gmail.com>
2025-07-16 17:13:42 +0900

    정리된 프로젝트 구조로 빌드 적용

--- vite-tailwind-portfolio/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class", // 다크모드 대응
-  plugins: [],
-};
