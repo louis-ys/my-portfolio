76f0e41d45a2e1ac072167ffd172e3beeb5877fd
louis-ys <lye98055@gmail.com>
2025-07-16 16:46:08 +0900

    빌드 결과물을 docs 폴더로 이동하여 GitHub Pages 배포

--- vite-tailwind-portfolio/vite.config.js
+++ vite-tailwind-portfolio/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-portfolio/",
+  build: {
+    outDir: "docs", // ✅ dist 대신 docs 폴더로 빌드
+  },
   plugins: [react()],
-  base: "/profile-louis/",
 });
